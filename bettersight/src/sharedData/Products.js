bafa470371db826667c96126b96dc8937b7071c1
Anis Dhaoui <freeconcept6@gmail.com>
2021-04-08 15:22:11 +0100

    Fixed the carousel by changing the courousel id in order to make every carousel working on its own

--- bettersight/src/sharedData/Products.js
+++ bettersight/src/sharedData/Products.js
@@ -7,5 +7,50 @@ export const products = [
         thirdImage: "images/product-1/3.jpg",
         price: "90.99",
         description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 1,
+        title: "Ray Ban",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 2,
+        title: "Ray Ban",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 3,
+        title: "Ray Ban",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 4,
+        title: "Ray Ban",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 5,
+        title: "Ray Ban",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
       }
     ]
