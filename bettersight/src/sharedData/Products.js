1f494e056d78cacd53a824838eee9afe83dba9e4
Anis Dhaoui <freeconcept6@gmail.com>
2021-04-15 03:41:36 +0100

    Adding Pagination using React-paginate package

--- bettersight/src/sharedData/Products.js
+++ bettersight/src/sharedData/Products.js
@@ -52,5 +52,248 @@ export const products = [
         thirdImage: "images/product-6/3.jpg",
         price: "90.99",
         description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 6,
+        title: "Lyndon",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 7,
+        title: "Gulfport",
+        defaultImage: "images/product-2/1.jpg",
+        secondImage: "images/product-2/2.jpg",
+        thirdImage: "images/product-2/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 8,
+        title: "Oberlin",
+        defaultImage: "images/product-3/1.jpg",
+        secondImage: "images/product-3/2.jpg",
+        thirdImage: "images/product-3/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 9,
+        title: "Haywood",
+        defaultImage: "images/product-4/1.jpg",
+        secondImage: "images/product-4/2.jpg",
+        thirdImage: "images/product-4/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 10,
+        title: "Burke",
+        defaultImage: "images/product-5/1.jpg",
+        secondImage: "images/product-5/2.jpg",
+        thirdImage: "images/product-5/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 11,
+        title: "Sioux",
+        defaultImage: "images/product-6/1.jpg",
+        secondImage: "images/product-6/2.jpg",
+        thirdImage: "images/product-6/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 12,
+        title: "Lyndon",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 13,
+        title: "Gulfport",
+        defaultImage: "images/product-2/1.jpg",
+        secondImage: "images/product-2/2.jpg",
+        thirdImage: "images/product-2/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 14,
+        title: "Oberlin",
+        defaultImage: "images/product-3/1.jpg",
+        secondImage: "images/product-3/2.jpg",
+        thirdImage: "images/product-3/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 15,
+        title: "Haywood",
+        defaultImage: "images/product-4/1.jpg",
+        secondImage: "images/product-4/2.jpg",
+        thirdImage: "images/product-4/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 16,
+        title: "Burke",
+        defaultImage: "images/product-5/1.jpg",
+        secondImage: "images/product-5/2.jpg",
+        thirdImage: "images/product-5/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 17,
+        title: "Sioux",
+        defaultImage: "images/product-6/1.jpg",
+        secondImage: "images/product-6/2.jpg",
+        thirdImage: "images/product-6/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 18,
+        title: "Lyndon",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 19,
+        title: "Gulfport",
+        defaultImage: "images/product-2/1.jpg",
+        secondImage: "images/product-2/2.jpg",
+        thirdImage: "images/product-2/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 20,
+        title: "Oberlin",
+        defaultImage: "images/product-3/1.jpg",
+        secondImage: "images/product-3/2.jpg",
+        thirdImage: "images/product-3/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 21,
+        title: "Haywood",
+        defaultImage: "images/product-4/1.jpg",
+        secondImage: "images/product-4/2.jpg",
+        thirdImage: "images/product-4/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 22,
+        title: "Burke",
+        defaultImage: "images/product-5/1.jpg",
+        secondImage: "images/product-5/2.jpg",
+        thirdImage: "images/product-5/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 23,
+        title: "Sioux",
+        defaultImage: "images/product-6/1.jpg",
+        secondImage: "images/product-6/2.jpg",
+        thirdImage: "images/product-6/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 24,
+        title: "Lyndon",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 25,
+        title: "Gulfport",
+        defaultImage: "images/product-2/1.jpg",
+        secondImage: "images/product-2/2.jpg",
+        thirdImage: "images/product-2/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 26,
+        title: "Oberlin",
+        defaultImage: "images/product-3/1.jpg",
+        secondImage: "images/product-3/2.jpg",
+        thirdImage: "images/product-3/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 27,
+        title: "Haywood",
+        defaultImage: "images/product-4/1.jpg",
+        secondImage: "images/product-4/2.jpg",
+        thirdImage: "images/product-4/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 28,
+        title: "Burke",
+        defaultImage: "images/product-5/1.jpg",
+        secondImage: "images/product-5/2.jpg",
+        thirdImage: "images/product-5/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 29,
+        title: "Sioux",
+        defaultImage: "images/product-6/1.jpg",
+        secondImage: "images/product-6/2.jpg",
+        thirdImage: "images/product-6/3.jpg",
+        price: "90.88",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 30,
+        title: "Lyndon",
+        defaultImage: "images/product-1/1.jpg",
+        secondImage: "images/product-1/2.jpg",
+        thirdImage: "images/product-1/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 31,
+        title: "Gulfport",
+        defaultImage: "images/product-2/1.jpg",
+        secondImage: "images/product-2/2.jpg",
+        thirdImage: "images/product-2/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
+      },
+      {
+        id: 32,
+        title: "Oberlin",
+        defaultImage: "images/product-3/1.jpg",
+        secondImage: "images/product-3/2.jpg",
+        thirdImage: "images/product-3/3.jpg",
+        price: "90.99",
+        description: "xxxxxxxxxxxxxxxxxxxxxxx"
       }
     ]
