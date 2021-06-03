330c6fb099e21322c587a79ee4f57da02b664997
Anis <freeconcept6@gmail.com>
2021-06-03 16:21:07 +0100

    Working on Testimonials carousel template

--- bettersight/src/Components/Aboutus.js
+++ bettersight/src/Components/Aboutus.js
@@ -54,24 +54,24 @@ export default function Aboutus (){
             </Col>
             <Col xs={12}>
                 <h1 className="text-center">Contact Address</h1><br />
-                <div class="row text-center">
-                    <div class="col-sm-3 col-xs-6 first-box">
-                        <h1><span class="glyphicon glyphicon-earphone"></span></h1>
+                <div className="row text-center">
+                    <div className="col-sm-3 col-xs-6 first-box">
+                        <h1><span className="glyphicon glyphicon-earphone"></span></h1>
                         <h3>Phone</h3>
                         <p>+080-1700-987654</p><br/>
                     </div>
-                    <div class="col-sm-3 col-xs-6 second-box">
-                        <h1><span class="glyphicon glyphicon-home"></span></h1>
+                    <div className="col-sm-3 col-xs-6 second-box">
+                        <h1><span className="glyphicon glyphicon-home"></span></h1>
                         <h3>Location</h3>
                         <p>1036 Voluptatum Road</p><br/>
                     </div>
-                    <div class="col-sm-3 col-xs-6 third-box">
-                        <h1><span class="glyphicon glyphicon-send"></span></h1>
+                    <div className="col-sm-3 col-xs-6 third-box">
+                        <h1><span className="glyphicon glyphicon-send"></span></h1>
                         <h3>E-mail</h3>
                         <p>info@yourdomain.com</p><br/>
                     </div>
-                    <div class="col-sm-3 col-xs-6 fourth-box">
-                        <h1><span class="glyphicon glyphicon-leaf"></span></h1>
+                    <div className="col-sm-3 col-xs-6 fourth-box">
+                        <h1><span className="glyphicon glyphicon-leaf"></span></h1>
                         <h3>Web</h3>
                         <p>www.bettersight.com</p><br/>
                     </div>
@@ -102,6 +102,141 @@ export default function Aboutus (){
                 </p>
             </Col>
           </Row>
+        </TabPane>
+        <TabPane tabId="3">
+            <section className="pt-5 pb-5">
+                <div id="testimonials" className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <h3 className="mb-3">Testimonials</h3>
+                        </div>
+                        <div className="col-6 text-right">
+                            <a className="btn btn-primary mb-3 mr-1" href="#feedbackCarousel" role="button" data-slide="prev">
+                                <i className="fa fa-arrow-left"></i>
+                            </a>
+                            <a className="btn btn-primary mb-3 " href="#feedbackCarousel" role="button" data-slide="next">
+                                <i className="fa fa-arrow-right"></i>
+                            </a>
+                        </div>
+                        <div className="col-12">
+                            <div id="feedbackCarousel" className="carousel slide" data-ride="carousel">
+
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <div className="row">
+
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532781914607-2031eca2f00d?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=7c625ea379640da3ef2e24f20df7ce8d" />
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Special title treatment</h5>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1517760444937-f6397edcbbcd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=42b2d9ae6feb9c4ff98b9133addfb698" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532712938310-34cb3982ef74?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3d2e8a2039c06dd26db977fe6ac6186a" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="row">
+
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532771098148-525cefe10c23?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=3f317c1f7a16116dec454fbc267dd8e4" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532715088550-62f09305f765?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=ebadb044b374504ef8e81bdec4d0e840" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1506197603052-3cc9c3a201bd?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=0754ab085804ae8a3b562548e6b4aa2e" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <div className="row">
+
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=ee8417f0ea2a50d53a12665820b54e23" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532777946373-b6783242f211?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=8ac55cf3a68785643998730839663129" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-md-4 mb-3">
+                                                <div className="card">
+                                                    <img className="img-fluid" alt="100%x280" src="https://images.unsplash.com/photo-1532763303805-529d595877c5?ixlib=rb-0.3.5&amp;q=80&amp;fm=jpg&amp;crop=entropy&amp;cs=tinysrgb&amp;w=1080&amp;fit=max&amp;ixid=eyJhcHBfaWQiOjMyMDc0fQ&amp;s=5ee4fd5d19b40f93eadb21871757eda6" />
+                                                    <div className="card-body">
+                                                        <h4 className="card-title">Special title treatment</h4>
+                                                        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </TabPane>
       </TabContent>
     </div>
